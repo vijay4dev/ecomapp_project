1e22ff2393a6e5c4c90b202a1ce28001cec4e37f
dev-tashvi <tashviyadav9@gmail.com>
2024-12-13 23:56:17 +0530

    splited the code and created filter

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,45 +1,62 @@
+import 'package:ecomapp_project/pages/shoe_filter.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final List<String> filters = const [
+    'ALL',
+    'Addidas',
+    'Nike',
+    'Puma',
+    'RedTape',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
                 Padding(
                   padding: EdgeInsets.all(20.0),
                   child: Text(
                     'Shoee\nCollection',
                     style: TextStyle(
-                      fontSize: 30,
+                      fontSize: 35,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      label: Text('Search'),
-                      contentPadding: EdgeInsets.all(20),
-                      prefixIcon: Icon(Icons.search),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1.5,color: Colors.black12),                        
-                        borderRadius: BorderRadius.only(topLeft:Radius.circular(40),bottomLeft: Radius.circular(40)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black12),                        
-                        borderRadius: BorderRadius.only(topLeft:Radius.circular(40),bottomLeft: Radius.circular(40)),
-                      ),
+                    child: TextField(
+                  decoration: InputDecoration(
+                    label: Text('Search'),
+                    contentPadding: EdgeInsets.all(20),
+                    prefixIcon: Icon(Icons.search),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1.5, color: Colors.black12),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          bottomLeft: Radius.circular(40)),
                     ),
-                  )
-                ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black45),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          bottomLeft: Radius.circular(40)),
+                    ),
+                  ),
+                )),
               ],
+            ),
+            SizedBox(
+              height: 120,
+              
+              child: ShoeFilter(filteritem: filters), 
             ),
           ],
         ),
---
+++ lib/pages/shoe_filter.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ShoeFilter extends StatefulWidget {
+  final List filteritem;
+
+  const ShoeFilter({
+    super.key,
+    required this.filteritem
+  });
+  @override
+  State<ShoeFilter> createState() => _ShoeFilterState();
+}
+class _ShoeFilterState extends State<ShoeFilter> {
+  String? selected;
+  @override
+  void initState() {
+    super.initState();
+    // Initialize 'selected' with the first item in the list
+    if (widget.filteritem.isNotEmpty) {
+      selected = widget.filteritem[0];
+    }
+  }
+  Widget build(BuildContext context) {
+    return   ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.filteritem.length,
+                itemBuilder: (context, index) {
+                  final filter = widget.filteritem[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {                          
+                            selected = filter;
+                        });
+                      },
+                      child: Chip(
+                        backgroundColor:selected == filter? Colors.amber[300]:const Color.fromARGB(162, 227, 242, 253),
+                        shape: RoundedRectangleBorder(
+                            side: const BorderSide(
+                                width: 0, color: Colors.transparent),
+                            borderRadius: BorderRadius.circular(30)),
+                        label: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            widget.filteritem[index],
+                            style: const TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+      );
+  }
+}
