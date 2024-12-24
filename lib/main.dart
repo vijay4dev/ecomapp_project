ddb6296b8d426e895c6357a5d1d8cfac1da4053e
dev-tashvi <tashviyadav9@gmail.com>
2024-12-24 08:13:27 +0530

    added flutter_native_splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:ecomapp_project/pages/cart_provider.dart';
 import 'package:ecomapp_project/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
-void main() {
+void main() async{
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 5));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
